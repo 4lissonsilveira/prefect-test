650086c6f5f4a60c9ca67586ef46756de8f55798
4lissonsilveira <assilveira@gmail.com>
2025-10-03 11:27:12 -0300

    ..

--- src/salesforce_erp_sync/erp_salesforce_sync_flows/case/clients/navision.py
+++ src/salesforce_erp_sync/erp_salesforce_sync_flows/case/clients/navision.py
@@ -13,5 +13,5 @@ class NavisionClient:
 
 def create_navision_client():
     #zeep_client = Client("navision_wsdl.xml")
-    zeep_client = Client("local_wsdl.xml")
+    zeep_client = Client("https://b6589f70effc.ngrok-free.app/wsdl")
     return NavisionClient(zeep_client)
